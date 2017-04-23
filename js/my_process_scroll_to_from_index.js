8f52b23e1dea41f00ad6177df7ea54ebaa0de66e
memodern <stepanlavrinenko@Stepans-MacBook-Air.local>
2017-04-23 16:35:04 -0600

    fix html->quote myprocess

---
+++ js/my_process_scroll_to_from_index.js
@@ -0,0 +1,7 @@
+$(document).ready(function(){
+      if (window.location.hash == "#collapse11") {
+        $('html, body').animate({
+           scrollTop: $("#moreInfo").offset().top
+         }, 1853);
+      }
+   });
