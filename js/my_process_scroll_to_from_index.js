cdd00b99defe6b3b53c63cb769f47e8f81f2cc92
memodern <stepanlavrinenko@Stepans-MacBook-Air.local>
2017-04-23 16:39:27 -0600

    fix myprocess links4

--- js/my_process_scroll_to_from_index.js
+++ js/my_process_scroll_to_from_index.js
@@ -2,6 +2,6 @@ $(document).ready(function(){
       if (window.location.hash == "#collapse11") {
         $('html, body').animate({
            scrollTop: $("#collapse11").offset().top
-         }, 1853);
+         }, 1853px);
       }
    });
