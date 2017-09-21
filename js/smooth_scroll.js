7babd4cc7e2c087609519ddf04f12e306417b69f
memodern <stepanlavrinenko@Stepans-MacBook-Air.local>
2017-09-21 13:21:05 -0600

    updated site with prev proj section completed

--- js/smooth_scroll.js
+++ js/smooth_scroll.js
@@ -1,3 +1,4 @@
 $('.index_withme_contactmebuttons').localScroll();
 $('.index_contactmehead_bg').localScroll();
 $('.index_myprocess_yellowcheck_link').localScroll();
+$('.nav_contact').localScroll();
